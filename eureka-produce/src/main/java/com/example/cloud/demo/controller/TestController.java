2669167eca95ba1bfc55121f811b45cc6cd98349
tr-y <756360729@qq.com>
2019-07-18 17:45:46 +0800

    add zuul config

--- eureka-produce/src/main/java/com/example/cloud/demo/controller/TestController.java
+++ eureka-produce/src/main/java/com/example/cloud/demo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.cloud.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -9,6 +11,7 @@ import java.util.Map;
 @RequestMapping(value = "get")
 public class TestController {
 
+    Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @RequestMapping(value = "test")
     public String getTest(@RequestParam(value = "name") String name){
@@ -20,7 +23,22 @@ public class TestController {
     public Map  getZuulTest(@RequestParam(value = "name") String name){
         Map  resultMap = new HashMap();
         resultMap.put("data",name);
+        resultMap.put("message","this is first produce");
         resultMap.put("isok","success");
         return resultMap;
+    }
+
+    @ResponseBody
+    @GetMapping(value = "ZuulRibbonTest")
+    public Map getZuulRibbonTest(){
+        Map resultMap = new HashMap();
+        resultMap.put("isok","true");
+        logger.info("this is  first service ");
+        try {
+            Thread.sleep(900);//默认zuul的超时时间是1000ms
+        } catch (InterruptedException e) {
+            resultMap.put("isok","fail");
+        }
+        return  resultMap;
     }
 }

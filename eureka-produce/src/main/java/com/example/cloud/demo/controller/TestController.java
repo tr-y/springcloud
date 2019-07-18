ca76516b7b5dad03fc93a34a776b2f5007dd2dbe
tr-y <756360729@qq.com>
2019-07-18 10:39:31 +0800

    produce alter

--- eureka-produce/src/main/java/com/example/cloud/demo/controller/TestController.java
+++ eureka-produce/src/main/java/com/example/cloud/demo/controller/TestController.java
@@ -1,22 +1,26 @@
 package com.example.cloud.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController(value = "get")
+@RestController
+@RequestMapping(value = "get")
 public class TestController {
 
 
     @RequestMapping(value = "test")
     public String getTest(@RequestParam(value = "name") String name){
+        return "produce"+name+" success ";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "getZuulTest")
+    public Map  getZuulTest(@RequestParam(value = "name") String name){
         Map  resultMap = new HashMap();
-        resultMap.put("data","success");
-        return "produce";
+        resultMap.put("data",name);
+        resultMap.put("isok","success");
+        return resultMap;
     }
 }
